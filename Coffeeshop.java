71ee6f13c061c377cdbf5760ed1278ea1dd40775
Luis R Guerrero <lrgutube09@gmail.com>
2023-01-14 21:05:05 +0000

    build coffee shop app

---
+++ Coffeeshop.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Coffeeshop{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        //ask the user for their balance
+        //ask the user if they want esspresso or americano
+        //ask the user how many they want
+        //if the user inputs 0 or negative number, produce no output
+        //if the user can not afford the number of coffees requested, produce no output
+        //the user should get a different message depending on the type of coffee they purchased
+        System.out.println("what's you balance?");
+        String balanceInput = sc.nextLine();
+        double balance = Double.parseDouble(balanceInput);
+        System.out.println("what type of coffee do you want?");
+        String coffeeInput = sc.nextLine();
+        System.out.println("how many do you want?");
+        String amountInput = sc.nextLine();   // or shortest way would be: int amountInput = Integer.parseInt(sc.nextLine());
+        int amount = Integer.parseInt(amountInput);
+        double espressoPrice = 3.99;
+        double americanoPrice = 2.99;
+
+        if (amount > 0){
+            //proceed with coffee purchase attemp?
+            boolean isEspresso = coffeeInput.equals("espresso");
+            boolean isAmericano = coffeeInput.equals("americano");
+            if(isEspresso){
+                if(balance >= (espressoPrice * amount)){
+                    //print out a successful espresso purchase message
+                    System.out.println("enjoy your espresso");
+                }else{
+                    System.out.println("you don't have enough balance");
+            }
+        }
+        if(isAmericano){
+            if(balance >= (americanoPrice * amount)){
+                //print out a successful espresso purchase message
+                System.out.println("enjoy your espresso");
+            }else{
+                System.out.println("you don't have enough balance");
+            }
+        }
+        }else{
+            System.out.println("no coffeee - amount requested is 0 or less");
+        }
+    }
+}
